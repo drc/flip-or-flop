076e4217377ae9104e34e2306ed1c939a6d5074f
Dan Cigrang <danielcigrang@me.com>
2020-11-14 15:49:10 -0600

    adding code to gen the csv and updated csv

---
+++ index.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require("fs");
+
+const url = "https://en.m.wikipedia.org/wiki/List_of_Flip_or_Flop_episodes";
+
+async function go() {
+    const { data: html } = await axios(url);
+    const { document } = new JSDOM(html).window;
+    const rows = Array.from(document.querySelectorAll("tr.vevent"));
+
+    const row_column = rows.map(row => Array.from(row.children));
+
+    let data = [
+        `"No. Overall","No. in season","Title","Original air date","Cost of home ($)","Closing cost ($)","Rehab cost ($)","Sale price ($)","Profit/loss ($)","Contractor"`,
+    ];
+
+    row_column.forEach(row => {
+        let rowdata = [];
+        row.forEach((column, idx) => {
+            0, 1, 4, 5, 6, 7, 8;
+            if (idx === 2 || idx === 9) {
+                rowdata.push(
+                    JSON.stringify(
+                        column.textContent.replace(/[+"]/g, "").trim()
+                    )
+                );
+            } else if (idx === 3) {
+                rowdata.push(new Date(column.textContent.trim()).toJSON());
+            } else {
+                rowdata.push(
+                    parseInt(column.textContent.replace(/[-+,]/g, "").trim())
+                );
+            }
+        });
+        if (rowdata.length === 10) data.push(rowdata.join());
+    });
+    fs.writeFileSync("data.csv", data.join("\n"), "utf-8");
+}
+
+go();
